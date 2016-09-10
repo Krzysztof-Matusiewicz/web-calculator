8da1022f3a8ed99df8b6f663900a7968e4af3d1f
Krzysztof Matusiewicz <k.matusiewicz@gmail.com>
2016-09-10 10:24:33 +0200

    Working Spring Boot app

---
+++ src/main/java/com/learning/webcalc/HelloController.java
@@ -0,0 +1,16 @@
+package com.learning.webcalc;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController
+{
+
+    @RequestMapping("/")
+    public String index()
+    {
+        return "Greetings from Spring Boot!1";
+    }
+
+}
---
+++ src/test/java/com/learning/webcalc/HelloControllerTest.java
@@ -0,0 +1,36 @@
+package com.learning.webcalc;
+
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest // TODO: try to replace by @WebMvcTest
+@AutoConfigureMockMvc
+public class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void getHello() throws Exception
+    {
+        mvc.perform(MockMvcRequestBuilders.get("/")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Greetings from Spring Boot!")));
+    }
+
+}
