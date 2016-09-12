82a40fe36ba60a3bb491b62d1a437f053f060a76
Krzysztof Matusiewicz <k.matusiewicz@gmail.com>
2016-09-12 20:58:03 +0200

    JavaScript refactoring

--- static/calculator.js
+++ static/calculator.js
@@ -1,111 +1,107 @@
 
-var output = "";
 var twoArgumentOperators = ["+", "-", "*", "/", "^"];
 
-function addToOutput(value) {
-    clearOutputIfContainsZero(value);
-    output += value;
-    updateOutput();
-    clearMessage();
-}
-
-function clearOutputIfContainsZero(newCharacter) {
-    if (output == "0" && twoArgumentOperators.indexOf(newCharacter) < 0) {
-        output = "";
-        updateOutput();
+var info = {
+    update : function(value) {
+        $("#info").html(value);
+    },
+    clear : function() {
+        info.update("");
     }
-}
+};
 
-function squareRoot() {
-    addToOutput("sqrt(");
-}
-
-function integral() {
-    addToOutput("integral(");
-}
-
-function removeLastOutputCharacter() {
-    if (output) {
-        if (output)
-        output = output.slice(0, -1);
-        if (!output) {
-            output = "0";
+var input = {
+    getValue : function() {
+        return $("#input").val()
+    },
+    setValue : function(value) {
+        $("#input").val(value);
+    },
+    clearIfContainsZero : function(newCharacter) {
+        if (input.getValue() == "0" && twoArgumentOperators.indexOf(newCharacter) < 0) {
+            input.setValue("");
         }
-        updateOutput();
-        clearMessage();
-    }
-}
-
-function clear() {
-    output = "0";
-    updateOutput();
-    clearMessage();
-}
-
-function calculate() {
-    $.getJSON("/calculate", {
-        exp: output
-    }).done(function(data) {
-        updateMessage(output);
-        output = String(data.result);
-        updateOutput();
-        if (isHistoryVisible()) {
-            updateHistory();
+    },
+    append : function(value) {
+        input.clearIfContainsZero(value);
+        input.setValue(input.getValue() + value);
+        info.clear();
+    },
+    removeLastOutputCharacter : function() {
+        var value = input.getValue();
+        if (value) {
+            value = value.slice(0, -1);
+            if (!value) {
+                value = "0";
+            }
+            input.setValue(value);
+            info.clear();
         }
-    }).fail(function(jqXHR) {
-        var data = jQuery.parseJSON(jqXHR.responseText);
-        updateMessage(data.message);
-    });
-}
-
-function isHistoryVisible() {
-    return $("#historyContainer").is(":visible");
-}
-
-function history() {
-    if (!isHistoryVisible()) {
-        updateHistory();
     }
-    $("#historyContainer").toggle();
-}
+};
 
-function updateHistory() {
-    $.getJSON("/history", {
-        exp: output
-    }).done(function(data) {
-        var historyHtml = "";
-        $.each(data.history.items.reverse(), function(i, item){
-            historyHtml += "<li>" + item.expression + " = " + item.result + "</li>";
+var historyPanel = {
+    isVisible : function() {
+        return $("#historyContainer").is(":visible");
+    },
+    update : function() {
+        $.getJSON("/history")
+            .done(function(data) {
+                var historyHtml = "";
+                $.each(data.history.items.reverse(), function(i, item){
+                    historyHtml += "<li>" + item.expression + " = " + item.result + "</li>";
+                });
+                $("#history").html(historyHtml);
+            }).fail(function() {
+            info.update("Cannot read calculation history");
         });
-        $("#history").html(historyHtml);
-    }).fail(function() {
-        updateMessage("Cannot read calculation history");
-    });
-}
+    },
+    toggle : function() {
+        if (!historyPanel.isVisible()) {
+            historyPanel.update();
+        }
+        $("#historyContainer").toggle();
+    }
+};
 
-function updateOutput() {
-    $("#result").text(output);
-}
-
-function updateMessage(value) {
-    $("#message").html(value);
-}
-
-function clearMessage() {
-    updateMessage("");
-}
+var calc = {
+    squareRoot : function() {
+        input.append("sqrt(");
+    },
+    integral : function() {
+        input.append("integral(");
+    },
+    clear : function() {
+        input.setValue("0");
+        info.clear();
+    },
+    execute : function() {
+        $.getJSON("/calculate", {
+            exp: input.getValue()
+        }).done(function(data) {
+            info.update(input.getValue());
+            input.setValue(String(data.result));
+            if (historyPanel.isVisible()) {
+                historyPanel.update();
+            }
+        }).fail(function(jqXHR) {
+            var data = jQuery.parseJSON(jqXHR.responseText);
+            info.update(data.message);
+        });
+    }
+};
 
 function init() {
     $("td.inputCharacter").bind("click", function () {
-        addToOutput($(this).text());
+        input.append($(this).text());
     });
-    $("td.squareRoot").bind("click", squareRoot);
-    $("td.integral").bind("click", integral);
-    $("td.backspace").bind("click", removeLastOutputCharacter);
-    $("td.calculate").bind("click", calculate);
-    $("td.history").bind("click", history);
-    $("td.clear").bind("click", clear);
-    clear();
+    $("td.squareRoot").bind("click", calc.squareRoot);
+    $("td.integral").bind("click", calc.integral);
+    $("td.backspace").bind("click", input.removeLastOutputCharacter);
+    $("td.calculate").bind("click", calc.execute);
+    $("td.history").bind("click", historyPanel.toggle);
+    $("td.clear").bind("click", calc.clear);
+    calc.clear();
     $("#historyContainer").hide();
 }
 
